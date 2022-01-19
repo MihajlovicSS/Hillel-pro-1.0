db18f90a56f2868cf514dc5332134984881df822
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2022-01-19 19:15:48 +0200

    Final-2

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let howOld = function(thisYear, yearOfBirthday){
 /*функция для выявления корректного окончания*/
 let correctWordOfAge = function(age){
     let result = '';
-    if(age%10 == 1){
+    if(age%10 == 1 && age != 11){
         result = 'год';
     }
     else if((age>1 && age<5) || (age>20 && (age%10==2 || age%10==3 || age%10==4))){
